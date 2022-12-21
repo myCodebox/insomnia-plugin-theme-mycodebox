a85cc4830464683ae9dc69d420e26f0311dec718
Spelunky <myCodebox@users.noreply.github.com>
2022-12-21 01:40:35 +0100

    init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# MyCodebox **Paraíso** for [Insomnia](http://insomnia.rest)
+
+> A dark theme for [Insomnia](http://insomnia.rest)
+
+![Screenshot](./screenshot.png)
+
+## Install
+
+* In Insomnia, go to Application and select Preferences
+* Click on Plugins
+* Paste `insomnia-plugin-theme-mycodebox` into the package name field
+* Click on Install Plugin
+
+## Activating theme
+
+* Go to Application and select Preferences
+* Click on Themes
+* Select Dracula to apply the theme
+
+
+## License
+
+[MIT License](./LICENSE)
---
+++ themes/mycodebox.js
@@ -0,0 +1,62 @@
+module.exports = {
+	name: "mycodebox",
+	displayName: 'Paraíso "mycbx"',
+	theme: {
+		background: {
+			default:  "#2f1e2e", // Primary
+			success:  "#48b685", // POST, 200 OK, parameter names
+			notice:   "#fec418", // PATCH
+			warning:  "#f99b15", // PUT
+			danger:   "#ef6155", // DELETE
+			surprise: "#e96ba8", // Dashboard link, GET, SEND button, branch button, add plugin button
+			info:     "#7a4d77", // OPTIONS, HEAD
+		},
+		foreground: {
+			default:  "#b29ab5", // Primary
+			success:  "#e7e9db", // Secondary font color
+			notice:   "#e7e9db", // Secondary font color
+			warning:  "#e7e9db", // Secondary font color
+			danger:   "#e7e9db", // Secondary font color
+			surprise: "#e7e9db", // Secondary font color
+			info:     "#b29ab5", // Secondary font color for info background
+		},
+		highlight: {
+			default: "#7a4d77",  // Sidebar highlight color
+			xxs:     "#231925",   // input background
+			xs:      "#472c4540", // Hover sidebar and Butons
+			sm:      "#472C45",   // Overlay border, Badges
+			md:      "#472c45",   // input border
+			lg:      "#e96ba880", // placeholder, Button border
+			xl:      "#7A5F78cc"    // Gutter Number color, Key
+		},
+
+		// The styles object targets sub-components of the Insomnia application.
+		styles: {
+			sidebar: {
+				background: {
+					default: "#231925",
+				},
+			},
+			dialog: {
+				background: {
+					default: "#2f1e2e",
+				},
+			},
+			paneHeader: {
+				background: {
+					success:  "#2F9D6C",
+					notice:   "#E5AB00",
+					warning:  "#E08200",
+					danger:   "#D6483C",
+					surprise: "#D0528F",
+					info:     "#61345E"
+				}
+			},
+			transparentOverlay: {
+				background: {
+					default: "#2f1e2ee6"
+				},
+			},
+		},
+	},
+};
